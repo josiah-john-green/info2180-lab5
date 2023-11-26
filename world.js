616dc5c850871b767b71000d0b0fa33bc8068508
Josiah-John Green <josiahjohngreen@gmail.com>
2023-11-26 17:03:57 -0500

    Edited files for Exercise 3

---
+++ world.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    // Wait for the DOM to be fully loaded
+
+    // Find the "Lookup" button by its ID
+    var lookupButton = document.getElementById('lookup');
+
+    // Find the input field by its ID
+    var countryInput = document.getElementById('country');
+
+    // Attach a click event listener to the button
+    lookupButton.addEventListener('click', performLookup);
+
+    // Attach a keypress event listener to the input field
+    countryInput.addEventListener('keypress', function(event) 
+    {
+    
+        // Check if the pressed key is "Enter"
+        if (event.key === 'Enter') 
+        {
+        
+            performLookup();
+        
+        }
+    
+    });
+
+    // Function to perform the lookup
+    function performLookup() 
+    {
+    
+        // Get the value entered by the user
+        var countryValue = countryInput.value;
+
+        // Create a new XMLHttpRequest object
+        var xhr = new XMLHttpRequest();
+
+        // Define the callback function to handle the response
+        xhr.onload = function() 
+        {
+        
+            // Find the result div by its ID
+            var resultDiv = document.getElementById('result');
+        
+            // Update the result div with the response from the server
+            resultDiv.innerHTML = xhr.responseText;
+        
+        };
+
+        // Construct the URL with the country parameter
+        var url = 'world.php?country=' + encodeURIComponent(countryValue);
+
+        // Open a GET request to the server
+        xhr.open('GET', url, true);
+
+        // Send the request
+        xhr.send();
+
+    }
+    
+});
--- world.php
+++ world.php
@@ -1,17 +1,52 @@
 <?php
-$host = 'localhost';
-$username = 'lab5_user';
-$password = '';
-$dbname = 'world';
 
-$conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+    $host = 'localhost';
+    $username = 'lab5_user';
+    $password = 'password123';
+    $dbname = 'world';
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    # Establish a connection to the database
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+
+    # Check if a country parameter is provided in the URL
+    if (isset($_GET['country'])) 
+    {
+
+        # Use prepared statement to prevent SQL injection
+        $countryParam = '%' . $_GET['country'] . '%';
+        
+        $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+
+        # Bind the parameter
+        $stmt->bindParam(':country', $countryParam, PDO::PARAM_STR);
+
+    } 
+    else 
+    {
+    
+        # If no country parameter, fetch all countries
+        $stmt = $conn->query("SELECT * FROM countries");
+    
+    }
+
+    # Execute the query
+    $stmt->execute();
+
+    # Fetch results
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+
+<!-- Output the results as an unordered list -->
+
 <ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
+    <?php 
+        foreach ($results as $row): 
+    ?>
+    
+        <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
+    
+    <?php 
+        endforeach; 
+    ?>
 </ul>
