9e13267e2d2e5e562c4060329daaaf28ebd11c91
Josiah-John Green <josiahjohngreen@gmail.com>
2023-11-27 07:14:49 -0500

    Edited files for Exercise 4

--- world.js
+++ world.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function () {
 
     // Wait for the DOM to be fully loaded
 
-    // Find the "Lookup" button by its ID
-    var lookupButton = document.getElementById('lookup');
+    // Find the "Lookup Country" button by its ID
+    var lookupButton = document.getElementById('lookupcntry');
 
     // Find the input field by its ID
     var countryInput = document.getElementById('country');
@@ -12,39 +12,35 @@ document.addEventListener("DOMContentLoaded", function() {
     lookupButton.addEventListener('click', performLookup);
 
     // Attach a keypress event listener to the input field
-    countryInput.addEventListener('keypress', function(event) 
-    {
-    
+    countryInput.addEventListener('keypress', function (event) {
+
         // Check if the pressed key is "Enter"
-        if (event.key === 'Enter') 
-        {
-        
+        if (event.key === 'Enter') {
+
             performLookup();
-        
+
         }
-    
+
     });
 
     // Function to perform the lookup
-    function performLookup() 
-    {
-    
+    function performLookup() {
+
         // Get the value entered by the user
-        var countryValue = countryInput.value;
+        var countryValue = countryInput.value.trim();
 
         // Create a new XMLHttpRequest object
         var xhr = new XMLHttpRequest();
 
         // Define the callback function to handle the response
-        xhr.onload = function() 
-        {
-        
+        xhr.onload = function () {
+
             // Find the result div by its ID
             var resultDiv = document.getElementById('result');
-        
+
             // Update the result div with the response from the server
             resultDiv.innerHTML = xhr.responseText;
-        
+
         };
 
         // Construct the URL with the country parameter
@@ -57,5 +53,5 @@ document.addEventListener("DOMContentLoaded", function() {
         xhr.send();
 
     }
-    
+
 });
--- world.php
+++ world.php
@@ -5,48 +5,67 @@
     $password = 'password123';
     $dbname = 'world';
 
-    # Establish a connection to the database
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
-    # Check if a country parameter is provided in the URL
+    # Check if the country parameter is set in the GET request
+
     if (isset($_GET['country'])) 
     {
 
-        # Use prepared statement to prevent SQL injection
-        $countryParam = '%' . $_GET['country'] . '%';
+        $country = $_GET['country'];
         
-        $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+        # Use a prepared statement to prevent SQL injection
 
-        # Bind the parameter
-        $stmt->bindParam(':country', $countryParam, PDO::PARAM_STR);
+        $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+        $stmt->bindValue(':country', '%' . $country . '%', PDO::PARAM_STR);
+        $stmt->execute();
+
+        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($results) 
+        {
+
+            # Output the results in an HTML table
+
+            echo '<table class="result-table">
+                    <thead>
+                        <tr>
+                            <th>Country Name</th>
+                            <th>Continent</th>
+                            <th>Independence Year</th>
+                            <th>Head of State</th>
+                        </tr>
+                    </thead>
+                    <tbody>';
+
+            foreach ($results as $row) 
+            {
+
+                echo '<tr>
+                        <td>' . $row['name'] . '</td>
+                        <td>' . $row['continent'] . '</td>
+                        <td>' . $row['independence_year'] . '</td>
+                        <td>' . $row['head_of_state'] . '</td>
+                    </tr>';
+
+            }
+
+            echo '</tbody></table>';
+            
+        } 
+        else 
+        {
+        
+            echo 'No matching countries found.';
+        
+        }
 
     } 
     else 
     {
-    
-        # If no country parameter, fetch all countries
-        $stmt = $conn->query("SELECT * FROM countries");
-    
+
+        echo 'Country parameter not set.';
+        
     }
 
-    # Execute the query
-    $stmt->execute();
-
-    # Fetch results
-    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 ?>
-
-<!-- Output the results as an unordered list -->
-
-<ul>
-    <?php 
-        foreach ($results as $row): 
-    ?>
-    
-        <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-    
-    <?php 
-        endforeach; 
-    ?>
-</ul>
