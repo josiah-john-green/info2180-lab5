d953dbb2ca0ba5f3c9aab30df476b86e2ba6293e
Josiah-John Green <josiahjohngreen@gmail.com>
2023-11-28 07:55:58 -0500

    Edited files for Exercise 5

--- world.js
+++ world.js
@@ -1,30 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    // Wait for the DOM to be fully loaded
-
     // Find the "Lookup Country" button by its ID
-    var lookupButton = document.getElementById('lookupcntry');
+    var lookupCountryButton = document.getElementById('lookupcntry');
+
+    // Find the "Lookup City" button by its ID
+    var lookupCityButton = document.getElementById('lookupcity');
 
     // Find the input field by its ID
     var countryInput = document.getElementById('country');
 
-    // Attach a click event listener to the button
-    lookupButton.addEventListener('click', performLookup);
+    // Attach click event listener to the "Lookup Country" button
+    lookupCountryButton.addEventListener('click', function () {
+        performLookup('country');
+    });
+
+    // Attach click event listener to the "Lookup City" button
+    lookupCityButton.addEventListener('click', function () {
+        performLookup('city');
+    });
 
     // Attach a keypress event listener to the input field
     countryInput.addEventListener('keypress', function (event) {
-
         // Check if the pressed key is "Enter"
         if (event.key === 'Enter') {
-
-            performLookup();
-
+            // Determine which button was clicked last and perform the lookup accordingly
+            if (lastClickedButton === 'country') {
+                performLookup('country');
+            } else {
+                performLookup('city');
+            }
         }
-
     });
 
+    // Variable to track the last clicked button
+    var lastClickedButton = 'country';
+
     // Function to perform the lookup
-    function performLookup() {
+    function performLookup(lookupType) {
+        // Update the last clicked button
+        lastClickedButton = lookupType;
 
         // Get the value entered by the user
         var countryValue = countryInput.value.trim();
@@ -34,24 +48,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Define the callback function to handle the response
         xhr.onload = function () {
-
             // Find the result div by its ID
             var resultDiv = document.getElementById('result');
-
             // Update the result div with the response from the server
             resultDiv.innerHTML = xhr.responseText;
-
         };
 
-        // Construct the URL with the country parameter
-        var url = 'world.php?country=' + encodeURIComponent(countryValue);
+        // Construct the URL with the country and lookup type parameters
+        var url = 'world.php?country=' + encodeURIComponent(countryValue) + '&lookup=' + encodeURIComponent(lookupType);
 
         // Open a GET request to the server
         xhr.open('GET', url, true);
 
         // Send the request
         xhr.send();
-
     }
 
 });
--- world.php
+++ world.php
@@ -8,64 +8,80 @@
     $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 
     # Check if the country parameter is set in the GET request
-
-    if (isset($_GET['country'])) 
-    {
-
+    if (isset($_GET['country'])) {
         $country = $_GET['country'];
-        
-        # Use a prepared statement to prevent SQL injection
 
-        $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
-        $stmt->bindValue(':country', '%' . $country . '%', PDO::PARAM_STR);
-        $stmt->execute();
-
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-        if ($results) 
-        {
-
-            # Output the results in an HTML table
-
-            echo '<table class="result-table">
-                    <thead>
-                        <tr>
-                            <th>Country Name</th>
-                            <th>Continent</th>
-                            <th>Independence Year</th>
-                            <th>Head of State</th>
-                        </tr>
-                    </thead>
-                    <tbody>';
-
-            foreach ($results as $row) 
-            {
-
-                echo '<tr>
-                        <td>' . $row['name'] . '</td>
-                        <td>' . $row['continent'] . '</td>
-                        <td>' . $row['independence_year'] . '</td>
-                        <td>' . $row['head_of_state'] . '</td>
-                    </tr>';
-
-            }
-
-            echo '</tbody></table>';
+        if (isset($_GET['lookup']) && $_GET['lookup'] === 'city') {
+            # City lookup
+            $stmt = $conn->prepare("SELECT * FROM cities WHERE country_code IN (SELECT code FROM countries WHERE name LIKE :country)");
+            $stmt->bindValue(':country', '%' . $country . '%', PDO::PARAM_STR);
             
-        } 
-        else 
-        {
-        
-            echo 'No matching countries found.';
-        
+            $stmt->execute();
+
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            if ($results) {
+                # Output the results in an HTML table for cities
+                echo '<table class="result-table">
+                        <thead>
+                            <tr>
+                                <th>City Name</th>
+                                <th>District</th>
+                                <th>Population</th>
+                            </tr>
+                        </thead>
+                        <tbody>';
+
+                foreach ($results as $row) {
+                    echo '<tr>
+                            <td>' . $row['name'] . '</td>
+                            <td>' . $row['district'] . '</td>
+                            <td>' . $row['population'] . '</td>
+                        </tr>';
+                }
+
+                echo '</tbody></table>';
+            } else {
+                echo 'No matching cities found.';
+            }
+        } else {
+            # Country lookup
+            $stmt = $conn->prepare("SELECT * FROM countries WHERE name LIKE :country");
+            $stmt->bindValue(':country', '%' . $country . '%', PDO::PARAM_STR);
+
+            $stmt->execute();
+
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            if ($results) {
+                # Output the results in an HTML table for countries
+                echo '<table class="result-table">
+                        <thead>
+                            <tr>
+                                <th>Country Name</th>
+                                <th>Continent</th>
+                                <th>Independence Year</th>
+                                <th>Head of State</th>
+                            </tr>
+                        </thead>
+                        <tbody>';
+
+                foreach ($results as $row) {
+                    echo '<tr>
+                            <td>' . $row['name'] . '</td>
+                            <td>' . $row['continent'] . '</td>
+                            <td>' . $row['independence_year'] . '</td>
+                            <td>' . $row['head_of_state'] . '</td>
+                        </tr>';
+                }
+
+                echo '</tbody></table>';
+            } else {
+                echo 'No matching countries found.';
+            }
         }
-
-    } 
-    else 
-    {
-
+    } else {
         echo 'Country parameter not set.';
-        
     }
 
 ?>
